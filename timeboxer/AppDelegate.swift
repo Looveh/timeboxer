fed4c532140947328bfd60818f785fb864ed5507
Niclas Nilsson <niclas.nilsson@protonmail.com>
2023-01-01 23:31:06 +0100

    Cleanup

--- timeboxer/AppDelegate.swift
+++ timeboxer/AppDelegate.swift
@@ -10,15 +10,11 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     private var window: NSWindow!
-    
     private var statusItem: NSStatusItem!
-    
     private var menuStartItem: NSMenuItem!
     private var menuStopItem: NSMenuItem!
-    
+    private var timer: Timer!
     private var endTime: Double!
-    
-    private var timer: Timer!;
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -33,13 +29,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         menuStartItem = NSMenuItem(
             title: "Start",
-            action: #selector(didTapStart),
+            action: #selector(onClickStart),
             keyEquivalent: "s"
         )
         
         menuStopItem = NSMenuItem(
             title: "Stop",
-            action: #selector(didTapStop),
+            action: #selector(onClickStop),
             keyEquivalent: "s"
         )
         menuStopItem?.isHidden = true
@@ -60,44 +56,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         timer = Timer.scheduledTimer(
             timeInterval: 1.0,
             target: self,
-            selector: #selector(timerTimeout),
+            selector: #selector(timerTick),
             userInfo: nil,
             repeats: true
         )
         
         menuStartItem?.isHidden = true
         menuStopItem?.isHidden = false
-    }
-    
-    func drawLabel() {
-        statusItem?.button?.title = countdownLabel()
-    }
-    
-    func countdownLabel() -> String {
-        if endTime == nil {
-            return "Timebox"
-        }
         
-        let diff = Int(endTime - NSDate().timeIntervalSince1970)
-        let hours = diff / 3600
-        let minutes = diff % 3600 / 60 + 1
-
-        if diff <= 0 {
-            return "Done"
-        } else if hours > 0 {
-            return "\(hours)h \(minutes)m"
-        } else {
-            return "\(minutes)m"
-        }
-    }
-    
-    @objc
-    func timerTimeout() {
         drawLabel()
-        
-        if NSDate().timeIntervalSince1970 >= endTime {
-            stopTimer()
-        }
     }
     
     func stopTimer() {
@@ -111,31 +78,62 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         drawLabel()
     }
     
+    func drawLabel() {
+        var label = ""
+        
+        if endTime == nil {
+            label = "Timebox"
+        } else {
+            let diff = Int(endTime - NSDate().timeIntervalSince1970)
+            let hours = diff / 3600
+            let minutes = diff % 3600 / 60 + 1
+            
+            if diff <= 0 {
+                label = "Done"
+            } else if hours > 0 {
+                label = "\(hours)h \(minutes)m"
+            } else {
+                label = "\(minutes)m"
+            }
+        }
+        
+        statusItem?.button?.title = label
+    }
+    
     @objc
-    func didTapStart() {
+    func timerTick() {
+        drawLabel()
+        
+        if NSDate().timeIntervalSince1970 >= endTime {
+            stopTimer()
+        }
+    }
+    
+    @objc
+    func onClickStart() {
+        let inputTextField = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 24))
+        inputTextField.placeholderString = ("minutes")
+        
         let alert = NSAlert()
         alert.messageText = "Timebox for"
         alert.addButton(withTitle: "Start")
         alert.addButton(withTitle: "Cancel")
-        
-        let inputTextField = NSTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 24))
-        inputTextField.placeholderString = ("minutes")
         alert.accessoryView = inputTextField
+        
         let response = alert.runModal()
         
+        // Didn't press "Start"
         if response != NSApplication.ModalResponse.alertFirstButtonReturn {
             return
         }
-
-        endTime = NSDate().timeIntervalSince1970 + inputTextField.doubleValue * 60;
+        
+        endTime = NSDate().timeIntervalSince1970 + inputTextField.doubleValue * 60
         
         startTimer()
-
-        drawLabel()
     }
     
     @objc
-    func didTapStop() {
+    func onClickStop() {
         endTime = nil
         
         stopTimer()
